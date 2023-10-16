456089dd278a568fd1f739c37fbcd43b1a6c32ab
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2023-10-16 09:11:27 -0700

    BC: Editted apps.py style to make it pep8 compliant

--- gce_portal/apps.py
+++ gce_portal/apps.py
@@ -1,37 +1,31 @@
+# Import packages
 import os
-# from django.conf import settings
 from django.apps import AppConfig
-
 from gce_portal import fields
 
 APP_DIR = os.path.join(os.path.dirname(__file__))
 
 
 class GCEPortalConfig(AppConfig):
-    name = 'gce_portal'
+    name = "gce_portal"
 
+# Details on the Globus Search index
 SEARCH_INDEXES = {
-    'gce': {
-        'uuid': 'cb2e5415-b5ee-49b0-b46d-3b33d44757a3',
-        'name': 'GCE Index',
-        'base_templates': 'globus-portal-framework/v2/',
-        'tabbed_project': False,
-        'access': 'private',
-        'template_override_dir': 'gce-portal',
-        'description': (
-            'Galactic Chemical Evolution (GCE) is a field of astronomy '
-            'that studies the origin and evolution of the elements '
-            'within galaxies.'
+    "gce": {
+        "uuid": "cb2e5415-b5ee-49b0-b46d-3b33d44757a3",
+        "name": "GCE Index",
+        "base_templates": "globus-portal-framework/v2/",
+        "tabbed_project": False,
+        "access": "private",
+        "template_override_dir": "gce-portal",
+        "description": (
+            "Galactic Chemical Evolution (GCE) is a field of astronomy "
+            "that studies the origin and evolution of the elements "
+            "within galaxies."
         ),
-        'facets': [
-          {
-            'name': 'Stellar yields (table)',
-            'field_name': 'table'
-          },
-          {
-            'name': 'Star formation efficiency (sfe)',
-            'field_name': 'sfe'
-          },
+        "facets": [
+            {"name": "Stellar yields (table)", "field_name": "table"},
+            {"name": "Star formation efficiency (sfe)", "field_name": "sfe"},
         ],
         "fields": [
             ("title", fields.title),
@@ -40,7 +34,9 @@ SEARCH_INDEXES = {
             ("preview_https_url", fields.preview_https_url),
             ("collection_id", fields.collection_id),
         ],
-        "sort": [{"field_name": "table", "order": "asc"},
-                 {"field_name": "sfe", "order": "asc"}],
+        "sort": [
+            {"field_name": "table", "order": "asc"},
+            {"field_name": "sfe", "order": "asc"},
+        ],
     }
 }
