6d2ba3c47d914417a593a040fd02b9c38722e3b0
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2023-10-16 09:13:37 -0700

    BC: Editted fields.py style to make it pep8 compliant

--- gce_portal/fields.py
+++ gce_portal/fields.py
@@ -1,49 +1,50 @@
 from urllib.parse import urlunsplit, urlencode
 
 # Guest Collection UUID where data is stored
-COLLECTION_ID = '96c76fb5-c70a-427c-a8d2-ea72eeff200b'
+COLLECTION_ID = "96c76fb5-c70a-427c-a8d2-ea72eeff200b"
+
 
 # Result title
-# ============
+# ------------
 def title(result):
-    '''Return the title of the current search result'''
+    """Return the title of the current search result"""
     return result[0]["directory"]
 
 
 # Globus App Link
-# ===============
+# ---------------
 def globus_app_link(result):
-    '''Return the URL to the Globus transfer web app'''
+    """Return the URL to the Globus transfer web app"""
 
     # Extract the path to the folder where the result is.
     # This must be from the root of the guest collection.
-    url = result[0]['directory']
+    url = result[0]["directory"]
 
     # Return the full URL that will redirect users to
     # the selected data, using the Globus web app.
-    query_params = {'origin_id': COLLECTION_ID,
-                    'origin_path':  url}
-    return urlunsplit(('https', 'app.globus.org', 'file-manager',
-                      urlencode(query_params), ''))
+    query_params = {"origin_id": COLLECTION_ID, "origin_path": url}
+    return urlunsplit(
+        ("https", "app.globus.org", "file-manager", urlencode(query_params), "")
+    )
 
 
 # HTTPS URL
-# =========
+# ---------
 def https_url(result):
-    '''Return a direct download link to files over HTTPS'''
-    path = result[0]['directory'] + "/" + result[0]['directory'] + ".npy"
+    """Return a direct download link to files over HTTPS"""
+    path = result[0]["directory"] + "/" + result[0]["directory"] + ".npy"
     return urlunsplit(("https", "g-6996b.fd635.8443.data.globus.org", path, "", ""))
 
 
 # Preview HTTPS URL
-# =================
+# -----------------
 def preview_https_url(result):
-    '''URL of the JSON preview data for HTTPS get requests'''
-    path = result[0]['directory'] + "/" + result[0]['directory'] + "_preview.json"
+    """URL of the JSON preview data for HTTPS get requests"""
+    path = result[0]["directory"] + "/" + result[0]["directory"] + "_preview.json"
     return urlunsplit(("https", "g-6996b.fd635.8443.data.globus.org", path, "", ""))
 
 
 # Collection ID
-# =============
+# -------------
 def collection_id(result):
     return COLLECTION_ID
